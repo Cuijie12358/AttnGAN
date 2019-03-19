7b2f6f79cad31d6e7ec2fa137ae453d7138524e2
Cuijie12358 <cuijie12358@outlook.com>
2019-03-19 17:02:18 +0000

    4 gpu

--- code/main.py
+++ code/main.py
@@ -26,7 +26,7 @@ def parse_args():
     parser.add_argument('--cfg', dest='cfg_file',
                         help='optional config file',
                         default='cfg/bird_attn2.yml', type=str)
-    parser.add_argument('--gpu', dest='gpu_id', type=int, default=-1)
+    parser.add_argument('--gpu', dest='gpu_num', type=int, default=-1)
     parser.add_argument('--data_dir', dest='data_dir', type=str, default='')
     parser.add_argument('--manualSeed', type=int, help='manual seed')
     args = parser.parse_args()
@@ -88,8 +88,8 @@ if __name__ == "__main__":
     if args.cfg_file is not None:
         cfg_from_file(args.cfg_file)
 
-    if args.gpu_id != -1:
-        cfg.GPU_ID = args.gpu_id
+    if args.gpu_num != 0:
+        cfg.GPU_NUM = args.gpu_num;
     else:
         cfg.CUDA = False
 
@@ -134,6 +134,7 @@ if __name__ == "__main__":
 
     # Define models and go to train/evaluate
     algo = trainer(output_dir, dataloader, dataset.n_words, dataset.ixtoword)
+    algo = nn.DataParallel(algo)
 
     start_t = time.time()
     if cfg.TRAIN.FLAG:
